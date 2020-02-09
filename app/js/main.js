6a949e0062ff8867769757f7cd2e07e77b4f1a0e
Arozhdan <halala30@gmail.com>
2020-02-09 16:42:13 +0100

    homepage

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,17 @@
 $(function(){
-
-
-
-  
+  $('.feedbacks-wrapper').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.feedbacks-nav'
+  });
+  $('.feedbacks-nav').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    asNavFor: '.feedbacks-wrapper',
+    dots: true,
+    centerMode: true,
+    focusOnSelect: true
+  });
 });
